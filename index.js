5efecfb0e51aef65810eed0a4f39584684d491dd
Madhav Wakhare <wakharemadhav@gmail.com>
2023-07-10 11:52:42 +0530

    4th commit

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(cors("*"));
 
 
 app.get("/emps", (request, response) => {
-  var queryToFire = "select * from Employee_Tb";
-  connection.query(queryToFire, (error, result) => {
+  var query = "select * from Employee_Tb";
+  connection.query(query, (error, result) => {
     if (error == null) {
       var dataInString = JSON.stringify(result);
       response.setHeader("Content-Type", "application/json");
@@ -29,20 +29,57 @@ app.get("/emps", (request, response) => {
   });
 });
 
-// app.post("/emps", (request, response) => {
-//   var queryToFire = "insert into Employee_Tb values(id,e_name,email,password,emp_id,dname,doj)";
-//   connection.query(queryToFire, (error, result) => {
-//     if (error == null) {
-//       var dataInString = JSON.stringify(result);
-//       response.setHeader("Content-Type", "application/json");
-//       response.send(dataInString);
-//     } else {
-//       console.log(error);
-//       response.setHeader("Content-Type", "application/json");
-//       response.send(error);
-//     }
-//   });
-// });
+app.delete("/emps/:emp_id", (request, response) => {
+  var queryToFire = `delete from Employee_Tb where id = ${request.params.emp_id}`;
+  connection.query(queryToFire, (error, result) => {
+    if (error == null) {
+      console.log(result);
+      var dataInString = JSON.stringify(result);
+      response.setHeader("Content-Type", "application/json");
+      response.send(dataInString);
+    } else {
+      console.log(error);
+      response.setHeader("Content-Type", "application/json");
+      response.send(error);
+    }
+  });
+});
+
+app.post("/emps/addemp", (request, response) => {
+  var queryToFire = `insert into Employee_Tb values ( ${request.body.id}, '${request.body.e_name}','${request.body.email}', '${request.body.password}', ${request.body.emp_id}, '${request.body.dname}', '${request.body.doj}' )`;
+
+  console.log(request.body);
+
+  connection.query(queryToFire, (error, result) => {
+    if (error == null) {
+      console.log(result);
+      var dataInString = JSON.stringify(result);
+      response.setHeader("Content-Type", "application/json");
+      response.send(dataInString);
+    } else {
+      response.setHeader("Content-Type", "application/json");
+      response.send(error);
+    }
+  });
+});
+
+app.put("/emps/:idreceived", (request, response) => {
+  var queryToFire = `update Employee_Tb set e_name = '${request.body.e_name}', email = '${request.body.email}', password = '${request.body.password}', emp_id = ${request.body.emp_id}, dname = '${request.body.dname}', doj = '${request.body.doj}'where id = ${request.params.idreceived} `;
+  console.log(request.body);
+  console.log(request.params);
+
+  connection.query(queryToFire, (error, result) => {
+    if (error == null) {
+      console.log(result);
+      var dataInString = JSON.stringify(result);
+      response.setHeader("Content-Type", "application/json");
+      response.send(dataInString);
+    } else {
+      response.setHeader("Content-Type", "application/json");
+      response.send(error);
+    }
+  });
+});
 
 
 
