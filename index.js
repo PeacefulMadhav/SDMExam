8702b51464aeaf4fc988c49cbe98bf8d413888fc
Madhav Wakhare <wakharemadhav@gmail.com>
2023-07-10 11:02:27 +0530

    2nd Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "manager",
+  database: "Emp",
+});
+
+const app = express();
+app.use(cors("*"));
+
+app.get("/", (request, response) => {
+  response.send("hello from node ");
+});
+
+app.get("/emps", (request, response) => {
+  var queryToFire = "select * from Employee_Tb";
+  connection.query(queryToFire, (error, result) => {
+    if (error == null) {
+      var dataInString = JSON.stringify(result);
+      response.setHeader("Content-Type", "application/json");
+      response.send(dataInString);
+    } else {
+      console.log(error);
+      response.setHeader("Content-Type", "application/json");
+      response.send(error);
+    }
+  });
+});
+
+app.post("/emps", (request, response) => {
+  var queryToFire = "insert into Employee_Tb";
+  connection.query(queryToFire, (error, result) => {
+    if (error == null) {
+      var dataInString = JSON.stringify(result);
+      response.setHeader("Content-Type", "application/json");
+      response.send(dataInString);
+    } else {
+      console.log(error);
+      response.setHeader("Content-Type", "application/json");
+      response.send(error);
+    }
+  });
+});
+
+
+
+app.listen(4000, "0.0.0.0", () => {
+  console.log("server started on port 4000");
+});
